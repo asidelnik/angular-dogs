54b7fc759638906026c1fd2e2bed5c57f959acbd
Amos <asidelnik@gmail.com>
2018-05-27 15:05:15 +0300

    ng-components - ex.2 middle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AddDogComponent } from './add-dog/add-dog.component';
+import { AddOwnerComponent } from './add-owner/add-owner.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddDogComponent,
+    AddOwnerComponent
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
-    FormsModule
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
