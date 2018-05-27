3a9b692cb7938be5bf98a74c39a14100dabddaff
Amos <asidelnik@gmail.com>
2018-05-27 13:48:55 +0300

    ng-intro - completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
-  title = 'my first angular 2.0 adventure!';
-  public dogName: string = 'Rex';
-  public dogWeight: number;
-  public dayDogPic: string = 'https://besthqwallpapers.com/Uploads/22-1-2018/37943/thumb2-4k-moon-dog-night-digital-art.jpg';
-  public ownerName: string;
+   title = 'my first angular 2.0 adventure!';
+   public dogName: string = 'Rex';
+   public dogWeight: number;
+   public ownerName: string;
+   public dayDogPic: string;
+
+   constructor() {
+      this.dayDogPic = this.imageUrl();
+   }
+   
+   imageUrl(): string{
+      var hr = (new Date()).getHours();
+      if (hr > 6 && hr < 20) {
+         return 'http://imgsrv.wkdzradio.com/image/wkdz4/UserFiles/Image/Dog%20Day1.jpg';
+      } else {
+         return 'https://besthqwallpapers.com/Uploads/22-1-2018/37943/thumb2-4k-moon-dog-night-digital-art.jpg';
+      }
+   }
 }
 
 //   imageUrl: ';
